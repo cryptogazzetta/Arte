1037b3a5c2b5093a4bf64012559c93fa90e17910
Mac <mac@MacBook-Air-de-Mac.local>
2023-11-14 15:12:38 -0300

    ..

--- extractors/catalogo_das_artes.py
+++ extractors/catalogo_das_artes.py
@@ -1,9 +1,12 @@
 # External modules
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 # Project Modules
 from utils import csv_handle
+from utils import safe_extract_functions
 
 
 def authenticate():
@@ -21,69 +24,75 @@ def authenticate():
 
     enter_button = driver.find_element(By.XPATH, "//button[@class='btn botao-invertido-fixo']")
     enter_button.click()
+
+    try:
+        WebDriverWait(driver, 10).until(lambda x: x.find_element(By.PARTIAL_LINK_TEXT, "SAIR"))
+    finally:
+        pass
+
     return driver
 
+
 def get_all_artworks_links():
+    # retrieve 'catalogo_das_artes_artworks_links.txt' file if it exists
+    try:
+        with open('./temporary-files/catalogo_das_artes_artworks_links.txt', 'r') as f:
+            links = f.readlines()
+        links = [link.strip() for link in links]
+    except:
+        links = []
+    
     driver = authenticate()
     page = 1
-    links = []
-    while page <= 13152:
-        paintings_url = 'https://www.catalogodasartes.com.br/cotacao/pinturas/ordem/inclusao_mais_recente/pagina/' + str(page) + '/'
-        driver.get(paintings_url)
 
+    links = []
+    while page <= 13158:
+        paintings_url = 'https://www.catalogodasartes.com.br/cotacao/pinturas/ordem/inclusao_mais_antiga/pagina/' + str(page) + '/'
+        driver.get(paintings_url)
         
         cards = driver.find_elements(By.XPATH, '//div[@class="card-image"]/a')
         try:
             new_links = [card.get_attribute('href') for card in cards]
-            links += new_links
         except:
             pass
         page += 1
 
+        links += new_links
+        links = list(set(links))
         if page % 10 == 0:
             with open('./temporary-files/catalogo_das_artes_artworks_links.txt', 'w') as f:
                 f.writelines([link + '\n' for link in links])
 
     return links
 
+
 def get_artwork_info(driver, link):
     driver.get(link)
-
-    html_content_dict = {}
-
-    # get image url
-    img_url = safe_extract(driver, '//img[contains(@class, "produto-imagem")]')
+    artwork_info = {}
+    artwork_info['url'] = link
+    
+    img_url = safe_extract_functions.safe_extract(driver, '//img[contains(@class, "produto-imagem")]')
     if img_url:
-        html_content_dict['img_url'] = img_url.get_attribute('src')
+        artwork_info['img_url'] = img_url.get_attribute('src')
 
     try:
-        # get data from table
-        table_element = safe_extract(driver, '//table[@class="produto-tabela"]')
+        table_element = safe_extract_functions.safe_extract(driver, '//table[@class="produto-tabela"]')
         rows = table_element.find_elements(By.TAG_NAME, 'tr')
         for row in rows:
             cells = row.find_elements(By.TAG_NAME, 'td')
             if len(cells) >= 2:
                 key = cells[0].text
                 value = cells[1].text
-                html_content_dict[key] = value
-    except:
+                # ignore new line characters
+                value = value.replace('\n', ' ')
+                artwork_info[key] = value
+        artwork_info['Failed'] = False
+    except Exception as e:
+        artwork_info['Failed'] = True
+        print(e)
         pass
-
-    return html_content_dict
-
-def safe_extract(driver, xpath):
-    try:
-        element = driver.find_element(By.XPATH, xpath)
-        return element.text
-    except:
-        return None
     
-def safe_extract_multiple(driver, xpath):
-    try:
-        elements = driver.find_elements(By.XPATH, xpath)
-        return elements
-    except:
-        return None
+    return artwork_info
 
 
 def get_all_artworks_info():
@@ -94,7 +103,7 @@ def get_all_artworks_info():
     driver = authenticate()
 
     artworks_info = []
-    batch_size = 100
+    batch_size = 10
     for artwork_link in artwork_links:
         artwork_info = get_artwork_info(driver, artwork_link)
         artworks_info.append(artwork_info)
