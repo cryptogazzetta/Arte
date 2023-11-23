90926742fca30545c378639d5715bc48342d1b8d
Mac <mac@MacBook-Air-de-Mac.local>
2023-11-23 13:13:33 -0300

    .

--- apps/price-suggest/back_end.py
+++ apps/price-suggest/back_end.py
@@ -20,7 +20,6 @@ def get_file_from_github(file_path, format='csv'):
         elif format == 'pkl':
             file_content = BytesIO(response.content)
         elif format in ['txt', 'css']:
-            print('a', format)
             file_content = response.text
 
         return file_content
